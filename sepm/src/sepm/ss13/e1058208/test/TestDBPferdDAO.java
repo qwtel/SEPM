b34be3d6f578ace3c3d357001d50bf574e243cd4
Florian Klampfer <f.klampfer@gmail.com>
2013-03-23 21:11:27 +0000

    basic test class for DBPferdDAO

---
+++ sepm/src/sepm/ss13/e1058208/test/TestDBPferdDAO.java
@@ -0,0 +1,33 @@
+package sepm.ss13.e1058208.test;
+
+import org.junit.*;
+
+
+public class TestDBPferdDAO {
+		
+	@Before	
+	public void setUp() throws Exception {
+		// Stelle Vorbedingungen fuer Testfaelle her
+		// (z.B.: Testdaten einfuegen)
+	}
+			
+	@After
+	public void tearDown() throws Exception {
+		// Stelle Ausgangszustand wieder her
+		// (z.B.: Testdaten loeschen)
+	}
+		
+	@Test
+	public void saveBook_shouldSaveBookToDB() {
+		// Testfall
+		// z.B.:
+		// speichere Buch, lese Buch
+		// ueberpruefe ob Buch korrekt gespeichert
+		// wurde (assert!)
+	}
+		
+	@Test
+	public void searchBook_shouldReturnSEBook() {
+		
+	}
+}
